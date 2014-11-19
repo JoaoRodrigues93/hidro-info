83baad8b82288278774526e71e72d75c0b2cb327
yoyogarife@gmail.com <yoyogarife@gmail.com@faa8c4c7-c26d-37da-4392-83851564d905>
2014-11-19 22:17:06 +0000

    ....

--- hidroinfo/src/mz/co/hidroinfo/dao/LeituraContadorDao.java
+++ hidroinfo/src/mz/co/hidroinfo/dao/LeituraContadorDao.java
@@ -1,11 +1,40 @@
 package mz.co.hidroinfo.dao;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.transform.DistinctRootEntityResultTransformer;
+
+import mz.co.hidroinfo.model.Contador;
 import mz.co.hidroinfo.model.LeituraContador;
+import mz.co.hidroinfo.model.Operador;
+import mz.co.hidroinfo.model.Pagamento;
 
 public class LeituraContadorDao extends GenericDAO<LeituraContador> {
 
 	public LeituraContadorDao() {
 		super(LeituraContador.class);
 	}
+	
+	public List<LeituraContador> pegaContador(int num) {
+
+		Session session = getSession();
+		Transaction tx = session.beginTransaction();
+		Criteria crit = session.createCriteria(LeituraContador.class);
+		Criteria cont=crit.createCriteria("contador");
+		cont.add(Restrictions.eq("numero", num));
+
+		List<LeituraContador> lista = crit.list();
+		DistinctRootEntityResultTransformer dist = DistinctRootEntityResultTransformer.INSTANCE;
+		List<LeituraContador> leitura = dist.transformList(lista);
+		tx.commit();
+		
+		return lista;
+	}
+
 	
 }
